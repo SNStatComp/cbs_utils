7850f5232d5fbea0b8a220f6bc8e3a15a41dcb78
eelco <eelcovv@gmail.com>
2018-09-08 09:46:50 +0200

    updated setup back to pyscaffold 3.0

--- setup.py
+++ setup.py
@@ -23,11 +23,8 @@ entry_points = """
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=[#'pyscaffold>=3.0a0,<3.1a0',
-                          'numpy',
-                          'pandas'] + sphinx,
+    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
           entry_points=entry_points,
-          version='0.4.0',
           use_pyscaffold=True)
 
 
